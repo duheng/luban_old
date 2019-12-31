5a3a50474ce0f82f57acbd79b6f9837282f45b92
shengyao <MY_NAME@example.com>
2019-12-31 22:40:51 +0800

    node

--- bin/source/react/app/constants/ActionTypes.js
+++
@@ -1,2 +0,0 @@
-
-export const RECEIVE_MOVIES = 'RECEIVE_MOVIES'
--- bin/webpack.config/postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    autoprefixer: { browsers: ['last 10 version'] },
-    precss: {},
-  },
-}
